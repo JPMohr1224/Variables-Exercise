e7cd7f0cd2acb7782fcbbb27e2ba1bf77b07e00d
JP Mohr <James.Mohr2.0@hotmail.com>
2024-04-26 10:38:00 -0400

    variables solutions 1

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "JP";
+            int myAge = 50;
+            char lastInitial = 'M';
+            bool isLearning = true;
+            double cash = 19;
+            decimal joke = 8.008m;
+
+            Console.WriteLine($"Hi there, my name is {myName} and I am {myAge}. My last name starts with {lastInitial}.");
+
         }
     }
 }
